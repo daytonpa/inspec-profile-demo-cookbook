52ad82b73ee4ac689f8128a41172f96569eb4f1d
Patrick Dayton <daytonpa@gmail.com>
2019-08-21 16:41:40 -0400

    Ready for demo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,6 @@ Initial release.
 - change 0
 - change 1
 
+# 1.0.1
+
+Ready for demo.  Cookbook fully installs [Node Exporter](https://github.com/prometheus/node_exporter)
--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,29 @@
 name 'inspec-profile-demo-cookbook'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Patrick Dayton'
+maintainer_email 'myemailis@notforyou.toobad'
 license 'All Rights Reserved'
 description 'Installs/Configures inspec-profile-demo-cookbook'
 long_description 'Installs/Configures inspec-profile-demo-cookbook'
-version '0.1.0'
+version '1.0.1'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/inspec-profile-demo-cookbook/issues'
+issues_url 'https://github.com/daytonpa/inspec-profile-demo-cookbook/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/inspec-profile-demo-cookbook'
+source_url 'https://github.com/daytonpa/inspec-profile-demo-cookbook'
+
+{
+  'ubuntu' => ['16.04', '18.04'],
+  'amazon' => ['2017.09', '2']
+}.each do |platform, platform_versions|
+  platform_versions.each do |version|
+    supports platform, version
+  end
+end
